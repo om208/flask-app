99c1005543dff64458f352f7a693fff14e389250
om208 <onkargaikwad208@gmail.com>
2024-09-22 13:08:52 +0530

    commenting the app.run two lies, - not needed in production

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
 # in production mode or git-hub don't push above 2 line 
